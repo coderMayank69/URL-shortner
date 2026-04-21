ef7fcbc21b64582cd48e407367ecf467cc46da19
coderMayank.69 <mayankbca96325@gmail.com>
2026-04-21 13:32:59 +0530

    chore: support live tunnel env config

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -6,9 +6,11 @@ const cookieSecure =
             ? false
             : process.env.NODE_ENV === "production";
 
+const cookieSameSite = process.env.COOKIE_SAME_SITE || "Lax";
+
 export const cookieOptions = {
     httpOnly: true,
     secure: cookieSecure,
-    sameSite: "Lax",
+    sameSite: cookieSameSite,
     maxAge: 1000 * 60 * 60, // 5 minutes
 }
